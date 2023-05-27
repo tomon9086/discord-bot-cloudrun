009ff61106784374240242eba8bee2e9a444cb73
tomon9086 <tomon9086@gmail.com>
2023-05-27 20:03:10 +0900

    install express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, World!')
+import express, { Request, Response } from 'express'
+
+const PORT = process.env.PORT ?? '8080'
+
+const app = express()
+
+app.get('/', async (_: Request, res: Response) => {
+  return res.status(200).send('Hello, World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
